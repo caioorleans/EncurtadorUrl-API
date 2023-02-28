8ecb3e2d91d6cd6c52650decd107abd243601b5f
caioorleans <caioorleansdm@hotmail.com>
2023-02-28 18:24:36 -0300

    Projeto Criado

---
+++ src/main/java/ncrtme/NcrtMeApplication.java
@@ -0,0 +1,13 @@
+package ncrtme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NcrtMeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NcrtMeApplication.class, args);
+	}
+
+}
---
+++ src/main/java/ncrtme/model/Url.java
@@ -0,0 +1,61 @@
+package ncrtme.model;
+
+import java.util.Date;
+
+import org.springframework.data.annotation.Id;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+
+@Entity
+public class Url {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private long id;
+	
+	@Column(nullable = false)
+	private String urlLonga; 
+	
+	@Column(nullable = false)
+	private Date dataCriacao;
+	
+	private Date dataExpiracao;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getUrlLonga() {
+		return urlLonga;
+	}
+
+	public void setUrlLonga(String urlLonga) {
+		this.urlLonga = urlLonga;
+	}
+
+	public Date getDataCriacao() {
+		return dataCriacao;
+	}
+
+	public void setDataCriacao(Date dataCriacao) {
+		this.dataCriacao = dataCriacao;
+	}
+
+	public Date getDataExpiracao() {
+		return dataExpiracao;
+	}
+
+	public void setDataExpiracao(Date dataExpiracao) {
+		this.dataExpiracao = dataExpiracao;
+	}
+	
+	
+	
+}
