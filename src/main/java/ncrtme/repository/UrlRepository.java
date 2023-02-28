4ee5b871af59b054831df75f875b97d45df68c1e
caioorleans <caioorleansdm@hotmail.com>
2023-02-28 18:48:32 -0300

    Criado o repository UrlRepository

---
+++ src/main/java/ncrtme/repository/UrlRepository.java
@@ -0,0 +1,8 @@
+package ncrtme.repository;
+
+import ncrtme.model.Url;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UrlRepository extends JpaRepository<Url, Long>{
+
+}
